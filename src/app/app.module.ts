ba6ab722b45fd4753be6e8c3e41fbc6ca308be56
Siddhika <siddhikamathur279@gmail.com>
2024-09-29 19:53:12 +0530

    initial submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { Section1Component } from './section1/section1.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Section1Component,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
